6758fe9926441b7983b338947ef85ea917898feb
Sahna Melly Marselina <sahnamm@gmail.com>
2022-01-27 22:20:41 +0700

    Initial Commit

---
+++ NBAPlayer/NBAPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAPlayerApp.swift
+//  NBAPlayer
+//
+//  Created by Sahna Melly Marselina on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct NBAPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
